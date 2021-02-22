45bc5efd10752d5408d5e38953015b4f2d5fb0a8
Jonathan Bockorny <jonathan.igor.pereira@hotmail.com>
2021-02-21 22:17:15 -0300

    Criando os Mocks dos verbos de rota get, post, put e delete

--- 01_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/Startup.cs
+++ 01_RestWithASPNETUdemy_Calculator/RestWithASPNETUdemy/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using RestWithASPNETUdemy.Services.Implementations;
 
 namespace RestWithASPNETUdemy
 {
@@ -28,6 +29,8 @@ namespace RestWithASPNETUdemy
         {
 
             services.AddControllers();
+            //Injeçãoo de Dependência
+            services.AddScoped<IPersonService, PersonServiceImplementation>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "RestWithASPNETUdemy", Version = "v1" });
